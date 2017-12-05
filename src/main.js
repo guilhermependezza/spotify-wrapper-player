f6ae9100a28ec12e9d292c75eb17d94d4f4c202c
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-05 00:47:41 -0200

    render the tracks

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
 import renderAlbumInfo from './AlbumInfo';
+import renderAlbumTracks from './AlbumTracks';
 
 const albums = spotify.search.albums('Incubus');
 const albumList = document.getElementById('album-list');
 const albumInfo = document.getElementById('album-info');
+const albumTracks = document.getElementById('album-musics');
 
 albums.then(data => data.json())
   .then((data) => {
@@ -13,4 +15,5 @@ albums.then(data => data.json())
   })
   .then(albumId => spotify.album.getAlbum(albumId))
   .then(albumDataRaw => albumDataRaw.json())
-  .then(albumData => renderAlbumInfo(albumData, albumInfo));
+  .then(albumData => renderAlbumInfo(albumData, albumInfo))
+  .then(({ tracks }) => renderAlbumTracks(tracks.items, albumTracks));
