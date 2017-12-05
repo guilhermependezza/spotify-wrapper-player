124b0cab97972734ac38807c7b605cd6311cc036
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-04 22:08:59 -0200

    Fix to use the correct image

--- src/AlbumInfo.js
+++ src/AlbumInfo.js
@@ -1,10 +1,10 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 
 const createMarkup = data => `
-    <img class="album-image" src="${data.images[1].url}" alt="">
+    <img class="album-image" src="${data.images[0].url}" alt="">
     <p class="album-title">${data.name}</p>
     <p class="album-artist">${data.artists[0].name}</p>
-    <p class="album-counter">${data.tracks.length} Músicas</p>`;
+    <p class="album-counter">${data.tracks.total} Músicas</p>`;
 
 export default function renderAlbumInfo(data, el) {
   const markup = createMarkup(data);
