074e905a3964a0cec0644ac3ea648a6f874475b7
Guilherme Pendezza de Sousa <guilherme_pendezza@hotmail.com>
2017-12-04 22:08:29 -0200

    Implement album info

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import spotify from './Spotify';
 import renderAlbums from './AlbumList';
+import renderAlbumInfo from './AlbumInfo';
 
 const albums = spotify.search.albums('Incubus');
 const albumList = document.getElementById('album-list');
+const albumInfo = document.getElementById('album-info');
 
 albums.then(data => data.json())
-  .then(data => renderAlbums(data.albums.items, albumList));
+  .then((data) => {
+    renderAlbums(data.albums.items, albumList);
+    return data.albums.items[0].id;
+  })
+  .then(albumId => spotify.album.getAlbum(albumId))
+  .then(albumDataRaw => albumDataRaw.json())
+  .then(albumData => renderAlbumInfo(albumData, albumInfo));
